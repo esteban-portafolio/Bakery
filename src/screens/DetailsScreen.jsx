aedbde9e84d43021b26f3eb4dc14f710f5473ca9
esteban-portafolio <e.mendoza.g93@gmail.com>
2023-02-01 23:39:39 -0300

    Bakery2

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 import React from "react";
 
-const DetailsScreen = () => {
+const DetailsScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>DetailsScreen</Text>
+      <Button title="Go to Categories" 
+      onPress={()=> navigation.popToTop()}/>
     </View>
   );
 };
